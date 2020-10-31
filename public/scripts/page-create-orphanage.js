5f10e48e30ed22963ce7ae8159d25fa4e88ed2a0
Lucas Pires <luc_50@outlook.com>
2020-10-31 02:25:43 -0300

    function button select yes/no-21ºcommit;

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -73,3 +73,21 @@ function deleteField(event) {
     span.parentNode.remove();
 
 }
+
+// selecionar sim ou não
+function toggleSelect(event){
+    // retirar a classe .active no botão clicado
+    document.querySelectorAll('.button-select button')
+    .forEach(function(button) {
+        button.classList.remove('active')
+    })
+
+    // colocar a classe .active no botão clicado
+    const button = event.currentTarget
+    button.classList.add('active');
+
+    // atualizar o meu input hidden com o valor selecionado
+    const input = document.querySelector('[name="open_on_weeksends"]')
+
+    input.value = button.dataset.value
+}
