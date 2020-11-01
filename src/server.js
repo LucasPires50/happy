880cd9cba8588e7f08ce649dcc921c448a8a2652
Lucas Pires <luc_50@outlook.com>
2020-10-31 23:49:13 -0300

    adequando ao hbs-29ºcommit;

--- src/server.js
+++ src/server.js
@@ -8,9 +8,12 @@ const server = express();
 server
     // utilizando os arquivos estáticos
     .use(express.static('public'))
+    //configurar template engine
+    .set('views', path.join(__dirname, 'views'))
+    .set('views engine', 'hbs')
     // criar uma rota 
     .get('/', (request, response) => {
-        return response.sendFile(path.join(__dirname, 'views', 'index.html'))
+        return response.render('index')
     })
 
 // ligar o servidor 
