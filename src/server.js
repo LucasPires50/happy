f9cb51aabcddf36c53126ee1e4ee6e6fd8a41e59
Lucas Pires <luc_50@outlook.com>
2020-10-31 23:12:48 -0300

    create servers-27ºcommit;

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 // [require] -> chama/importar as depedências 
 const express = require('express');
+const path = require('path');
 
 // iniciando o express(biblioteca)
 const server = express();
 
-// criar uma rota 
-server.get('/', () => {
-    console.log('oi')
-})
+server
+    // utilizando os arquivos estáticos
+    .use(express.static('public'))
+    // criar uma rota 
+    .get('/', (request, response) => {
+        return response.sendFile(path.join(__dirname, 'views', 'index.html'))
+    })
 
 // ligar o servidor 
 server.listen(5590)
